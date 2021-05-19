a4d5aec42d068236ca045674dfde5c1d3e5c060b
BL-Jotiram <jotiram.bandgar@bridgelabz.com>
2021-05-19 12:07:15 +0530

    ADDED: functionality to check emp present or not

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 
 public class EmployeeWageBuilder {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to emp wage computation program");
+        final int IS_PRESENT = 1;
+        double empCheck = Math.floor((Math.random() * 10)) % 2;
+        System.out.println(IS_PRESENT);
+        if(empCheck == IS_PRESENT){
+            System.out.println("employee is present");
+        }else {
+            System.out.println("employee is absent");
+        }
+
     }
 }
